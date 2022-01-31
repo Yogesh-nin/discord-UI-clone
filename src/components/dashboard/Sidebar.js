43169ce98d9a1c5fafc1412b974250fc809ac3ce
Yogesh <yogesh.204081@sggscc.ac.in>
2022-01-31 13:27:29 +0530

    adding sidebox

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -2,13 +2,15 @@ import React from 'react';
 import favicon from '../../images/favicon.ico'
 import new_line from '../../images/new_line.png'
 import plus_icon from '../../images/plus.png'
+import Sidebox from './Sidebox'
 
 const Sidebar = () => {
   return(
+
     <div className="container-fluid">
     <div className="row flex-nowrap">
-        <div className="col-auto px-sm-2 px-0 bg-dark">
-            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+        <div className="col-auto px-sm-2 px-0" style={{backgroundColor: '#202225'}}>
+            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 min-vh-100 my-3">
                 <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto">
                     <img className='rounded-4' src={favicon} alt="" height='50px'/>
                 </a>
@@ -16,20 +18,19 @@ const Sidebar = () => {
                     <img className='rounded-4' src={new_line} alt="" height='50px' />
                 </a>
                     <a href="/" className="d-flex align-items-center">
-                        <div className='rounded-circle d-flex justify-content-center align-items-center' style={{height: "50px", width: '50px'}}>
-                            <img className='' src={plus_icon} alt="" height='15px'/>
+                        <div className='rounded-circle d-flex justify-content-center align-items-center' style={{ backgroundColor: '#4f545c' , height: "50px", width: '50px'}}>
+                            <img className='' src={plus_icon} alt="" height='20px'/>
                         </div>
-                    </a>
-                
-                
-                
-                
-                
+                    </a>    
                 
             </div>
+          
         </div>
+        <Sidebox />
     </div>
 </div>
+ 
+  
   )
 };
 
---
+++ src/components/dashboard/Sidebox.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import friend from '../../images/friends.png'
+import stage_discovery from '../../images/stage_discovery.png'
+import nitro from '../../images/nitro_icon.png'
+import plus_icon from '../../images/plus_icon.png'
+const Sidebox = () => {
+  return(
+              <div className='col-2' style={{backgroundColor: '#2f3136', color: '#b9bbbe'}}>
+                  <form className='my-2'>
+                      <input type="text" placeholder='Find or start a conversation' className='form-control' id='searchBox' />
+                  </form>
+                  <div className='my-1'>
+                      <img src={friend} /> Friends
+                  </div>
+                  <div className='my-1'>
+                      <img src={stage_discovery} /> Stage Discovery
+                  </div>
+                  <div className='my-1'>
+                      <img src={nitro} /> Nitro
+                  </div>
+
+                  <div className='my-4'>
+                      <div>
+                          DIRECT MESSAGES
+                          <img src={plus_icon} alt="" style={{float: 'right'}} />
+                      </div>
+                          
+                  </div>
+              </div>
+          
+
+  )
+};
+
+export default Sidebox;

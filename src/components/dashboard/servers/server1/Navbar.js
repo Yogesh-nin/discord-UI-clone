90828754a16e540b7d7e7d027486fe751c76cbc9
Yogesh <yogesh.204081@sggscc.ac.in>
2022-02-09 20:33:24 +0530

    navbar

--- src/components/dashboard/servers/server1/Navbar.js
+++ src/components/dashboard/servers/server1/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import q_mark from '../../../../images/question_mark_icon.png'
 const Navbar = () =>{
     return(
         <div className="navbar navbar-expand-lg">
@@ -8,9 +8,35 @@ const Navbar = () =>{
                     <div className='mx-3'>
                         <i className="fas fa-hashtag"> &nbsp;</i>
                         <span style={{fontSize: '18px', fontWeight: '600'}}>Javascript</span> 
-                    </div>
-                
+                    </div>    
                 </div>
+
+                <div className='col'>
+                    <div className='row d-flex justify-content-end'>
+                        <div className='col-auto'>
+                            <div className='fs-5'>
+                                <span className='mx-2'><i class="fas fa-bell"></i></span>
+                                <span className='mx-2'><i class="fas fa-thumbtack"></i></span>
+                                <span className='mx-2'><i class="fas fa-user-friends"></i></span>
+                            </div>
+                        </div>
+
+                        <div className='col-auto'>
+                            <div class="input-group" style={{backgroundColor: '#292b2f'}}>
+                                <input type="text" className="form-control border-0 m-0 text-white" placeholder="Search" style={{backgroundColor: '#292b2f'}}/>
+                                <div className="input-group-append d-flex align-items-center mx-2" >
+                                    <i class='fas fa-search'></i>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-auto fs-5'>
+                            <span className='mx-2'><i class="fas fa-question-circle"></i></span>
+                        </div>
+                    </div>
+                </div>
+
+                
+
             </div>
         </div>
     )

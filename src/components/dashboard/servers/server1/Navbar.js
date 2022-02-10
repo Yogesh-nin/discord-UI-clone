5eeb109c5a2450312eccf8e8a98115d715c03530
Yogesh <yogesh.204081@sggscc.ac.in>
2022-02-10 14:00:19 +0530

    refactoring navbar

--- src/components/dashboard/servers/server1/Navbar.js
+++ src/components/dashboard/servers/server1/Navbar.js
@@ -2,12 +2,14 @@ import React from 'react'
 import q_mark from '../../../../images/question_mark_icon.png'
 const Navbar = () =>{
     return(
-        <div className="navbar navbar-expand-lg">
-            <div className="row text-white align-items-center">
-                <div className="col-auto nav-brand mx-2 my-1">
+        <div>
+        <div className="navbar navbar-expand-lg" >
+            <div className='container-fluid'>
+            <div className="row text-white align-items-center g-0" style={{width: 'inherit'}}>
+                <div className="col-auto mx-2 my-1">
                     <div className='mx-3'>
                         <i className="fas fa-hashtag"> &nbsp;</i>
-                        <span style={{fontSize: '18px', fontWeight: '600'}}>Javascript</span> 
+                        <span style={{fontSize: '18px', fontWeight: '600'}}>Javascript</span>
                     </div>    
                 </div>
 
@@ -38,6 +40,8 @@ const Navbar = () =>{
                 
 
             </div>
+            </div>
+        </div>
         </div>
     )
 }

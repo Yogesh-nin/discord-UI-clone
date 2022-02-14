a3b6a49e952ea24dd73cd6b84869abc13e33b9e4
Yogesh <yogesh.204081@sggscc.ac.in>
2022-02-14 17:26:55 +0530

    refactoring rightbox

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -11,14 +11,14 @@ import MessageBox from './servers/server1/MessageBox';
 
 const Dashboard = () => {
   return(
-      <div className='container-fluid'>
+      <div className='container-fluid vh-100'>
           <div className='row'>
               <Sidebar />
               <Sidebox />
-              <div className="col vh-100 g-0" style={{backgroundColor: '#36393f', color:'#8e9297' }}>
+              <div className="col g-0" style={{backgroundColor: '#36393f', color:'#8e9297' }}>
                 {/* <Navbar_middle /> */}
                 <Navbar />
-                <div className='row flex-nowrap g-0'>
+                <div className='row d-flex flex-nowrap g-0'>
                   {/* <OnlineFriend />
                   <AddFriend /> */}
                   
--- src/components/dashboard/servers/server1/RightBox.js
+++ src/components/dashboard/servers/server1/RightBox.js
@@ -3,15 +3,38 @@ import icon from '../../../../images/disc_icon.png'
 
 const RightBox = () =>{
     return(
-        <>
             <div className='col' style={{backgroundColor: '#2f3136', color: '#ffffff'}}>
                 <div className='my-4 mx-3'>
                     <h6 style={{fontSize: '15px'}}>ONLINE-3</h6>
                 </div>
 
                 <div className='mx-4'>
-                    <div className='d-flex'>
+                    <div className='d-flex my-1'>
                         <div className='rounded-circle d-flex justify-content-center align-items-center' style={{backgroundColor: '#ed4245', height: "50px", width: '50px'}}>
+                            <img src={icon} alt="" height='20px' />
+                        </div>
+                        <div className='flex-grow-1 d-flex align-items-center mx-2'>
+                            yogesh
+                        </div>
+                    </div>
+                    <div className='d-flex my-1'>
+                        <div className='rounded-circle d-flex justify-content-center align-items-center' style={{backgroundColor: '#3ba55d', height: "50px", width: '50px'}}>
+                            <img src={icon} alt="" height='20px' />
+                        </div>
+                        <div className='flex-grow-1 d-flex align-items-center mx-2'>
+                            yogesh
+                        </div>
+                    </div>
+                    <div className='d-flex my-1'>
+                        <div className='rounded-circle d-flex justify-content-center align-items-center' style={{backgroundColor: '#f26522', height: "50px", width: '50px'}}>
+                            <img src={icon} alt="" height='20px' />
+                        </div>
+                        <div className='flex-grow-1 d-flex align-items-center mx-2'>
+                            yogesh
+                        </div>
+                    </div>
+                    <div className='d-flex my-1'>
+                        <div className='rounded-circle d-flex justify-content-center align-items-center' style={{backgroundColor: '#f26522', height: "50px", width: '50px'}}>
                             <img src={icon} alt="" height='20px' />
                         </div>
                         <div className='flex-grow-1 d-flex align-items-center mx-2'>
@@ -20,9 +43,7 @@ const RightBox = () =>{
                     </div>
                 </div>
             </div>
-            
-            
-        </>
+        
     )
 }
 

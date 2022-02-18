2653665c7a1faa983b994916abafbfc9aa022075
Yogesh <yogesh.204081@sggscc.ac.in>
2022-02-18 16:58:45 +0530

    adding styling in navbar and header

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,42 +1,65 @@
 import React from "react";
 import logo from "../../images/disc_icon.png";
-function Navbar() {
+import './Navbar.css'
+const Navbar = () =>{
   return (
     <nav className="navbar navbar-expand-lg navbar-light">
       <div className="container-fluid">
-          <div className="container my-2">
-        <div className="row d-flex justify-content-between text-white">
-          <div className="col-2">
-            <a className="navbar-brand text-white" href="/"><img className="d-inline-block align-center" src={logo} alt="logo" width="28px" height="20px" /> Discord</a>
+        <div className="container my-4 container_content">
+          <div className="row d-flex justify-content-between text-white px-4">
+            <div className="col-2">
+              <a className="navbar-brand text-white" href="/">
+                <img
+                  className="d-inline-block align-center"
+                  src={logo}
+                  alt="logo"
+                  width="28px"
+                  height="20px"
+                />
+                &nbsp; Discord
+              </a>
+            </div>
+            <div className="col-8">
+              <ul className="navbar-nav mr-auto justify-content-center">
+                <li className="nav-item active">
+                  <a className="nav-link  text-white" href="#">
+                    Download
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link  text-white" href="#">
+                    Nitro
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link  text-white" href="#">
+                    Safety
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-white" href="#">
+                    Support
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link  text-white" href="#">
+                    Blog
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link  text-white" href="#">
+                    Careers
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-2 justify-content-center">
+              <button type="button" className="btn btn-light rounded-pill">
+                {" "}
+                Login{" "}
+              </button>
+            </div>
           </div>
-          <div className="col-8">
-                <ul className="navbar-nav mr-auto justify-content-center">
-                    <li className="nav-item active">
-                        <a className="nav-link  text-white" href="#">Download</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link  text-white" href="#">Nitro</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link  text-white" href="#">Safety</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link text-white" href="#">Support</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link  text-white" href="#">Blog</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link  text-white" href="#">Careers</a>
-                    </li>
-                
-                
-                </ul>
-          </div>
-          <div className="col-2 justify-content-center">
-            <button type="button" className="btn btn-light rounded-pill"> Login </button>
-          </div>
-        </div>
         </div>
       </div>
     </nav>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,18 +11,28 @@ function Header(){
             <div className='header_container'>
                 <Navbar />
 
-                <div className='row d-flex justify-content-center'>
-                    <div className='col-6 middlePart text-center'>
+                <div className='row d-flex justify-content-center middlePart '>
+                    <div className='col-6 text-center'>
                         <h1 className='heading'>IMAGINE A PLACE...</h1>
-                        <div className="my-4">
+                        <div className="my-4 px-3 middle_para">
                             <p className='text-center'>...where you can belong to a school club, a gaming group and a worldwide art community.Where just you and handful of friends can spend time together. A place that makes it easy to talk everyday and hang out more often</p>
                         </div>
+
+                        <div className="row buttons_row">
+                          <div className="col-5 offset-1 d-grid">
+                              <a href='#' className="btn btn-light rounded-pill btn-block btn-lg"> Download for mac </a>
+                          </div>
+                          <div className="col-5 d-grid">
+                              <a href='#' className="btn btn-dark rounded-pill btn-lg"> open discord in browser </a>
+                          </div>
+                      </div>
                     </div>
                 </div>
                 
                 <div className='back_foregroundImages'>
                     <img className='center_background_image' src={center_bg} alt="backgroundImage" />
                     <img className='left_foreground' src={left_bg} alt='left foreground' width='689px' height='352px' />
+                    <img className='right_foreground' src={right_bg} alt='right foreground' width='615px' height='360px' />
                 </div>
             </div>
 

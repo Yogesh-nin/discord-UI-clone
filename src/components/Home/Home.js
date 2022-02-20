cc8cbb5735a2180317d914c3380a5e4319e97092
Yogesh <yogesh.204081@sggscc.ac.in>
2022-02-20 16:39:02 +0530

    refactoring and style Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
                   <p>Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat. </p>
               </div>
           </div>
-          <div className="row row-content row_hanging_out d-flex align-items-center">
+          <div className="row row-content row-alternate d-flex align-items-center">
               <div className="col-6 col-md-3">
                   <h1 className='description_heading' >Where hanging-out is easy</h1>
                   <p>Grab a seat in a voice channel when you're free. Friends in your servercan see you're around and instantly pop in to talk without having to call.</p>
@@ -41,12 +41,21 @@ const Home = () => {
               </div>
           </div>
 
-          <div className='row row-content'>
+          <div className='row row-content row-alternate'>
               <div className="col">
-                <h1 className='text-center description_heading'>RELIABLE TECH FOR STAYING CLOSE</h1>
-                <p>Low-latency voice and video feels like you're in the same room. Wave hello over video, watch friends stream their game gather up and have a drawing session with screen share.</p>
-                <img src={fourth_img} alt="Reliable tech " width='678px' height='440px'/>
+                  <div className='row d-flex justify-content-center'>
+                        <div className='col-9'>
+                        <h1 className='description_heading'>RELIABLE TECH FOR STAYING CLOSE</h1>
+                        <p className='text-center'>Low-latency voice and video feels like you're in the same room. Wave hello over video, watch friends stream their game gather up and have a drawing session with screen share.</p>
+                      </div>
 
+                  </div>
+
+                  <div className='row'>
+                      <div className='col-auto'>
+                      <img src={fourth_img} alt="Reliable tech " width='1180px' height='716px'/>
+                      </div>
+                  </div>
               </div>
           </div>
 

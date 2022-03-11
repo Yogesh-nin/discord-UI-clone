29b58e05058c52cceadb149c31276653e6bc17bd
Yogesh-nin <002chandra.yogesh@gmail.com>
2022-03-11 13:20:21 +0530

    deploy

--- src/components/dashboard/components/Friend/OnlineFriend/OnlineFriend.js
+++ src/components/dashboard/components/Friend/OnlineFriend/OnlineFriend.js
@@ -5,7 +5,7 @@ const OnlineFriend = () => {
   return(
     <>
     
-      <div className='col-8 d-flex justify-content-center align-items-center online'>
+      <div className='col-8 d-flex justify-content-center align-items-center online mid-col'>
         <div className='my-auto'>
           <img src={no_friend} alt="no_one_online" width='415px' height='200px' />
           <p className='justify-content-center'>No one's around around to play with wumpus</p>
